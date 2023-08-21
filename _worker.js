bc463380e62024731de9b7bb2ff33ff2e1dc3710
xiaoxxiao <113592485+lemongac@users.noreply.github.com>
2023-08-21 14:51:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,28 +1,50 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    const url = new URL(request.url);
-    const actualUrlStr = url.pathname.replace("/proxy/","") + url.search + url.hash
-  
-    const actualUrl = new URL(actualUrlStr)
-  
-    const modifiedRequest = new Request(actualUrl, {
-      headers: {
-        ...request.headers,
-        'Host': actualUrl.hostname
-      },
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
-  
-    const response = await fetch(modifiedRequest);
-    const modifiedResponse = new Response(response.body, response);
-  
-    // 添加允许跨域访问的响应头
-    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-  
-    return modifiedResponse;
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
   }
+}
+
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+
+    let urlsStr = url.pathname.startsWith('/proxy/') ? url.pathname.replace("/proxy/","") : url.pathname.replace("/tproxy/","")
+    let urls = urlsStr.split(',');
+
+    shuffleArray(urls);
+
+    const domain = url.searchParams.get('domain');
+
+    if (url.pathname.startsWith('/proxy/') || url.pathname.startsWith('/tproxy/')) {
+      for(let actualUrlStr of urls){
+        let actualUrl = new URL(actualUrlStr);
+
+        let modifiedRequest = new Request(actualUrl, {
+          headers: request.headers,
+          method: request.method,
+          body: request.body,
+          redirect: 'follow'
+        });
+
+        if (domain) {
+          modifiedRequest.headers.set('domain', domain);
+        }
+     
+        try {
+          const response = await fetch(modifiedRequest);
+          const modifiedResponse = new Response(response.body, response);
+          modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+          modifiedResponse.headers.set('actualUrl', actualUrlStr);
+          return modifiedResponse;
+
+        } catch (error) {
+          console.error(error.message);
+          continue;
+        }
+      } 
+    }
+
+    return new Response('All URLs failed', { status: 500 });
+  },
+};
